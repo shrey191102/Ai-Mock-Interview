2971773960fd083ac1c4595f0406f7261bc5b0d2
shrey191102 <shreyanshsingh0019@gmail.com>
2025-10-26 20:43:17 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
+
+export default function Home() {
+  return (
+
+    <div>
+    {/* Header */}
+    <Header />
+    {/* Hero */}
+    <Hero/>
+    
+    </div>
+  );
+}
